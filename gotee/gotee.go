b62619efd40de3a572e36af7404a61c4a3f03203
Alan Diaz <adiazny@gmail.com>
2020-12-23 15:35:12 -0500

    removed src/

--- gotee/gotee.go
+++ gotee/gotee.go
@@ -22,7 +22,6 @@ func main() {
 	if argsLength > 0 {
 		if args[0] == "-a" {
 			canAppend = true
-			// argsLength--
 			index++
 		}
 
@@ -44,15 +43,15 @@ func main() {
 
 // CreateFile returns a file with append or no append ability
 func CreateFile(fileName string, canAppend bool) *os.File {
-	if canAppend {
-		file, err := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-		if err != nil {
-			log.Fatal(err)
-		}
-		return file
-	}
+	var err error
+	var file *os.File
 
-	file, err := os.OpenFile(fileName, os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0644)
+	if canAppend {
+		file, err = os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	} else {
+
+		file, err = os.OpenFile(fileName, os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0644)
+	}
 	if err != nil {
 		log.Fatal(err)
 	}
